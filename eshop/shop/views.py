5ccf5e3412bd20415b25b2b34cb3547606920671
Alex Bazarov <bazaroffalex@gmail.com>
2025-09-26 14:08:14 +0300

    Create urls for models: product, category. Add absolute urls

--- eshop/shop/views.py
+++ eshop/shop/views.py
@@ -3,13 +3,21 @@ from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
 
 def product_list(request, category_slug=None):
+    """
+    View products on page
+    if category_slug define,
+    show products in selected category
+    """
     category = None
     categories = Category.objects.all()
     products = Product.objects.filter(available=True)
+    
+    # define category
     if category_slug:
         category = get_object_or_404(Category,
                                      slug=category_slug)
         products = products.filter(category=category)
+
     return render(request,
                   'shop/product/product_list.html',
                   {'category': category,
@@ -17,6 +25,9 @@ def product_list(request, category_slug=None):
                    'products': products})
 
 def product_detail(request, id, slug):
+    """
+    View product's details
+    """
     product = get_object_or_404(Product,
                                 id=id,
                                 slug=slug,
