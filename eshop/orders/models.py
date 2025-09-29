05d704116a3f1e71c0867d1d6480f84991aac77c
Alex Bazarov <bazaroffalex@gmail.com>
2025-09-29 16:35:45 +0300

    create models Order, OrderItem

---
+++ eshop/orders/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+
+from shop.models import Product
+
+class Order(models.Model):
+    """
+    Serve for users orders
+    """
+    city = models.CharField(max_length=30)
+    address = models.CharField(max_length=150)
+    postal_code = models.CharField(max_length=10)
+
+    paid = models.BooleanField(default=False)
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created']
+        indexes = [
+            models.Index(fields=['-created'])
+        ]
+
+    def __str__(self):
+        return f'Order {self.id}'
+
+    def get_total_cost(self):
+        " return total cost of order "
+        return sum(item.get_cost()
+                   for item in self.items.all())
+
+class OrderItem(models.Model):
+    """
+    Content order items
+    """
+    order = models.ForeignKey(Order,
+                              related_name='items',
+                              on_delete=models.CASCADE)
+    product = models.ForeignKey(Product,
+                                related_name='order_items',
+                                on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10,
+                                decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
+    
+    def __str__(self):
+        return (self.id)
+    
+    def get_cost(self):
+        " return cost of items in the position "
+        return (self.price * self.quantity)
