ec1e07f59f2d7290be1fa01fd0faa59fb0fdbc3f
Alex Bazarov <bazaroffalex@gmail.com>
2025-10-16 23:32:10 +0300

    feat(account): add pagination for order_list

--- eshop/account/user_orders.py
+++ eshop/account/user_orders.py
@@ -4,6 +4,7 @@ Views for user orders
 """
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 
 from orders.models import Order, OrderItem
 
@@ -13,6 +14,9 @@ def order_list(request):
     Check orders placed by current user
     """
     orders = Order.objects.filter(user=request.user)
+    paginator = Paginator(orders, 30)
+    page_num = request.GET.get('page')
+    orders = paginator.get_page(page_num)
     return render(request,
                   'account/orders/order_list.html',
                   {'orders': orders})
